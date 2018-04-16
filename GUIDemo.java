8c40d7757dfab5103ea83d6961a27163199c4ee0
Ritter <ritternj@l14358.cs.jmu.edu>
2018-04-16 15:13:56 -0400

    added a move button to GUIDemo.java to randomly move the JPanel position

--- GUIDemo.java
+++ GUIDemo.java
@@ -15,6 +15,7 @@ public class GUIDemo extends JFrame
     private JPanel panel;
     private JButton biggerButton;
     private JButton smallerButton;
+    private JButton moveButton;
 
     /**
      * Set up the application.
@@ -27,11 +28,14 @@ public class GUIDemo extends JFrame
         panel = new JPanel();
         biggerButton = new JButton("BIGGER");
         smallerButton = new JButton("SMALLER");
+        moveButton = new JButton("MOVE");
+        moveButton.addActionListener(new ButtonHandler());
         biggerButton.addActionListener(new ButtonHandler());
         smallerButton.addActionListener(new ButtonHandler());
         add(panel);
         panel.add(biggerButton);
         panel.add(smallerButton);
+        panel.add(moveButton);
         setVisible(true);
     }
 
@@ -53,9 +57,13 @@ public class GUIDemo extends JFrame
             {
                 setSize(size.width + 10, size.height + 10);
             }
-            else
+            else if (e.getSource().equals(smallerButton))
             {
                 setSize(size.width - 10, size.height - 10);
+            }
+            else
+            {
+                setLocation((int)(Math.random() * 1000), (int)(Math.random() * 1000));
             }
 
         }
